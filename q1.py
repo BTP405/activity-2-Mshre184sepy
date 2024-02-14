e72566d29920c3b3a84af01325f4bbd18c7c296a
Mshre184sepy <158525739+Mshre184sepy@users.noreply.github.com>
2024-02-14 13:49:19 -0500

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,1 +1,22 @@
+# Question1
+# Write a Python function getPrimeNumbers(n) which returns a list containing all prime numbers between 2 and n.
+# Create a helper function to determine if a particular number is prime and then use a comprehension to generate your list.
+
+def is_prime(number):
+    """Checking if a number is prime or not."""
+    if number < 2:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
 def getPrimeNumbers(n):
+    """Returns a list of prime numbers between the range 2,n"""
+    return [Num for Num in range(2, n + 1) if is_prime(Num)]
+
+# testing
+n = 44
+prime_numbers = getPrimeNumbers(n)
+print(f"Prime numbers between 2 and {n}: {prime_numbers}")
+
